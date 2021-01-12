52a07251f1cd5f34abcb07e4d95f6b4af8ea71c6
chaoting <02115214@gm.scu.edu.tw>
2021-01-12 21:30:48 +0800

    2020.01.12 21:30

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import './App.css';
+// import './App.css';
 
 import React from 'react';
 import BMIApp from './components/BMIApp';
-
-// import { fab } from '@fortawesome/free-brands-svg-icons';
-// import { far } from '@fortawesome/free-regular-svg-icons';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// library.add(fab, fas, far);
 
 function App() {
   return (
